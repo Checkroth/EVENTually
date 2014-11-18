4f94f89b9bc013b1bfff8e5742dc72202cc8d56c
Charles <checkroth@gmail.com>
2014-11-19 01:49:38 +0800

    Setting up some urls and adding apps to settings

--- eventually/eventually/settings.py
+++ eventually/eventually/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'events',
+    'subevents',
 )
 
 MIDDLEWARE_CLASSES = (
--- eventually/eventually/urls.py
+++ eventually/eventually/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eventually.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    (r'', include('accounts.urls')),
+    (r'', include('events.urls')),
+    (r'', include('subevents.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
