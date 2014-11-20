1fb6eceac2fae9d096df96cd10949012a986cdb0
Alyxis Johnson <alyxis.c.johnson@gmail.com>
2014-11-19 20:44:52 -0500

    Added css, images, & js files

---
+++ eventually/staticfiles/javascripts/bucket.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    // CALENDAR
+
+    $('#calendar').fullCalendar({
+        header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+        defaultDate: '2014-11-12',
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+
+    })
+    
+    // EVENT SCROLLER
+    
+     $("#scroller").simplyScroll({
+        	   auto: false,
+			 speed: 5
+     });
+    
+    // SIDR MOBILE NAV
+    
+    $('#sidr-menu').sidr();	
+    $('#sidr-menu').click(function(){
+	   var $this = $('body'); //has to be the container of all the content which may differ per design
+		  if( $this.is('.fixed') ) {
+	           $this.removeClass('fixed');
+	       }
+	       else {
+		      $this.addClass('fixed');
+	       }
+	   });	
+	   
+    
+});
+
+
+                 
