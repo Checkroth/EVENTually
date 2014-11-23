9fdc3497accd07c3dae7c0aaff244656c6f25cfe
Jon Thompson <jt@Jons-MacBook-Pro.local>
2014-11-23 14:13:45 -0500

    On homepage, redirect to login if not logged in

--- eventually/static_pages/views.py
+++ eventually/static_pages/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 import events
 
 # Create your views here.
 def dashboard(request):
-    user = request.user
-    my_events = events.models.Event.objects.filter(host=user)
+    try:
+        user = request.user
+        my_events = events.models.Event.objects.filter(host=user)
+    except:
+        return redirect('/accounts/login');
 
     return render(request, 'static_pages/dashboard.html', {
         'user': user,
