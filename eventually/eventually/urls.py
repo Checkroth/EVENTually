bf5efc277d936dee319ec0075ef97f3a76cec8b0
Jon Thompson <jt@Jons-MacBook-Pro.local>
2014-11-23 14:34:52 -0500

    Redirect to home page on login

--- eventually/eventually/urls.py
+++ eventually/eventually/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import settings
 urlpatterns = patterns('',
-    # (r'', include('accounts.urls')),
+    (r'', include('accounts.urls')),
     (r'', include('events.urls')),
     (r'', include('static_pages.urls')),
     # (r'', include('subevents.urls')),
