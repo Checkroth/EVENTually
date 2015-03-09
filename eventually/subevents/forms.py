d63d9917eaa18d54fb10880cc2a816c7d422f5df
Charles Heckroth <checkroth@gmail.com>
2015-03-09 15:54:01 -0400

    Fixing stupid capitalization issues

--- eventually/subevents/forms.py
+++ eventually/subevents/forms.py
@@ -3,10 +3,10 @@ import subevents
 from django.core.files.images import get_image_dimensions
 
 #No clue if this actually works
-class SubEventForm(forms.ModelForm):
+class SubeventForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None) 
-        super(SubEventForm, self).__init__(*args, **kwargs)
+        super(SubeventForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = subevents.models.Subevent
