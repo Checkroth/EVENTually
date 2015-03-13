86982034527d1e0b547b23ab1382bdf0b42f541b
Alyxis Johnson <alyxis.c.johnson@gmail.com>
2015-03-13 01:46:21 -0400

    fixed paths & added about page to urls

--- eventually/static_pages/urls.py
+++ eventually/static_pages/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('static_pages.views',
     url(r'^$', 'dashboard', name='dashboard'),
+    url(r'^$', 'about', name='about'),
 )
