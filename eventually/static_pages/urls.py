a05749f3750e26c8145bbb4b6ab887266e3231b5
Alyxis Johnson <alyxis.c.johnson@gmail.com>
2015-03-13 01:51:45 -0400

    removed line from this file

--- eventually/static_pages/urls.py
+++ eventually/static_pages/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('static_pages.views',
     url(r'^$', 'dashboard', name='dashboard'),
-    url(r'^$', 'about', name='about'),
 )
