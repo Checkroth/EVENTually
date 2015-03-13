a0c84e6d369b58fb9e7597ddf9fef5e1455f8138
Alyxis Johnson <alyxis.c.johnson@gmail.com>
2015-03-12 21:56:44 -0400

    styling edits

--- eventually/staticfiles/javascripts/bucket.js
+++ eventually/staticfiles/javascripts/bucket.js
@@ -1,38 +1,22 @@
 $(document).ready(function() {
 
-    // CALENDAR
-
-    $('#calendar').fullCalendar({
-        header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-        defaultDate: '2014-11-12',
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-
-    })
-    
-    // EVENT SCROLLER
-    
-     $("#scroller").simplyScroll({
-        	   auto: false,
-			 speed: 5
-     });
     
     // SIDR MOBILE NAV
     
-    $('#sidr-menu').sidr();	
-    $('#sidr-menu').click(function(){
-	   var $this = $('body'); //has to be the container of all the content which may differ per design
-		  if( $this.is('.fixed') ) {
-	           $this.removeClass('fixed');
-	       }
-	       else {
-		      $this.addClass('fixed');
-	       }
-	   });	
+    //$('#sidr-menu').sidr();	
+    //$('#sidr-menu').click(function(){
+	  // var $this = $('body'); //has to be the container of all the content which may differ per design
+		//  if( $this.is('.fixed') ) {
+	      //     $this.removeClass('fixed');
+	       //}
+	       //else {
+		     // $this.addClass('fixed');
+//	       }
+//	   });
+    
+    $('header nav').meanmenu({
+        meanScreenWidth: "768"
+    });
 	   
     
 });
