3dd69698330e63bed137b19faa4b1d00c0667dcc
Charles Heckroth <checkroth@gmail.com>
2015-03-15 18:19:44 -0400

    Setting up about page rendering

--- eventually/accounts/urls.py
+++ eventually/accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('accounts.views',
     url(r'^accounts/signup/$', 'create_account', name='create_account'),
     url(r'^accounts/profile/$', 'profile', name='profile'),
     url(r'^accounts/confirmation/$', 'confirmation', name='confirmation'),
+    url(r'^accounts/login/$', 'login', name='login'),
 )
--- eventually/static_pages/urls.py
+++ eventually/static_pages/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('static_pages.views',
-    url(r'^$', 'dashboard', name='dashboard'),
+    url(r'^dashboard/$', 'dashboard', name='dashboard'),
+    url(r'^$', 'about', name='about'),
 )
